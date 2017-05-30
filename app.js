4875489f002918d0ac0b951c0f1f698d022b1bb5
anujashelke <anuja_shelke@outlook.com>
2017-05-30 15:29:43 +0530

    submitted

---
+++ app.js
@@ -0,0 +1,11 @@
+var name = "Anuja";
+
+var DOB = "7/9/1991";
+
+var BirthPlace = "Amravati";
+
+document.write("NAME :" + name + "<br />");
+
+document.write("DOB :" + DOB + "<br />");
+
+document.write("Place of Birth :" + BirthPlace + "<br />");
